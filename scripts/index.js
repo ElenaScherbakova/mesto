638af7768530559cd04bcd5855fa6b3ed5d7c145
Elena Scherbakova <25pochta25@mail.ru>
2022-10-06 14:55:35 +0300

    changed styles classes

--- scripts/index.js
+++ scripts/index.js
@@ -6,33 +6,31 @@ const form = document.querySelector(".popup__text");
 const nameInputName = document.querySelector(".popup__input_name");
 const nameInputAvocation = document.querySelector(".popup__input_avocation");
 const nameSpan = document.querySelector(".profile__title");
-const saveBtn = document.querySelector(".popup__form-text");
+const saveBtn = document.querySelector(".popup__button");
 const avocationSpan = document.querySelector(".profile__subtitle");
 
 
 openPopupBtn.addEventListener("click", () => {
-  popup.style.display = "block";
+  popup.classList.add("popup__opened");
   const name = nameSpan.innerText;
-  nameInputName.value = name;
   const avocation = avocationSpan.innerText;
+  nameInputName.value = name;
   nameInputAvocation.value = avocation;
 });
 
 popupCloseBtn.addEventListener("click", () => {
-  popup.style.display = "none";
+  popup.classList.remove("popup__opened");
 });
 
 saveBtn.addEventListener("click", () => {
-  popup.style.display = "none";
+  popup.classList.remove("popup__opened");
   const name = nameInputName.value;
+  const avocation = nameInputAvocation.value;
   nameSpan.innerText = name;
+  avocationSpan.innerText = avocation;
+
 });
 
-saveBtn.addEventListener("click", () => {
-  popup.style.display = "none";
-  const avocation = nameInputAvocation.value;
-  avocationSpan.innerText = avocation;
-});
 
 
 
