0fc6230bbadb9cfadf141d5053a23e3165779bd4
Elena Scherbakova <25pochta25@mail.ru>
2022-10-07 15:37:31 +0300

    changed styles classes

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,12 @@
 const openPopupBtn = document.querySelector(".profile__edit");
 const popup = document.querySelector(".popup");
-const popupContainer = popup.querySelector(".popup__container");
 const popupCloseBtn  = popup.querySelector(".popup__close");
-const nameInputName = document.querySelector(".popup__input_name");
-const nameInputAvocation = document.querySelector(".popup__input_avocation");
+const nameInputName = document.getElementById("name");
+const nameInputAvocation = document.getElementById("avocation");
 const nameSpan = document.querySelector(".profile__title");
-const saveBtn = document.querySelector(".popup__button");
 const avocationSpan = document.querySelector(".profile__subtitle");
 const form  = document.getElementById("submit-form");
+
 const submitListener = (formEvent) => {
   formEvent.preventDefault();
   popup.classList.remove("popup__opened");
@@ -15,8 +14,12 @@ const submitListener = (formEvent) => {
   const avocation = nameInputAvocation.value;
   nameSpan.innerText = name;
   avocationSpan.innerText = avocation;
-
 };
+
+const closeBtnListener = () => {
+  popup.classList.remove("popup__opened");
+};
+
 const openBtnListener = () => {
   popup.classList.add("popup__opened");
   const name = nameSpan.innerText;
@@ -25,16 +28,6 @@ const openBtnListener = () => {
   nameInputAvocation.value = avocation;
 };
 
-
 openPopupBtn.addEventListener("click", openBtnListener);
-
-popupCloseBtn.addEventListener("click", () => {
-  popup.classList.remove("popup__opened");
-});
-
+popupCloseBtn.addEventListener("click", closeBtnListener);
 form.addEventListener("submit", submitListener);
-
-
-
-
-
