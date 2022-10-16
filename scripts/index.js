7d3cdc481964d52d3d401034358f688a356050bf
Elena Scherbakova <25pochta25@mail.ru>
2022-10-16 18:11:16 +0300

    added basket

--- scripts/index.js
+++ scripts/index.js
@@ -1,108 +1,157 @@
 const openPopupBtn = document.querySelector(".profile__edit");
-const popup = document.querySelector(".popup");
-const popupCloseBtn  = popup.querySelector(".popup__close");
+const openPopupTitleBtn = document.querySelector(".profile__plus");
+const popupEdit = document.querySelector(".popup__edit");
+const popupPlus = document.querySelector(".popup__plus");
+const popupEditSave  = popupEdit.querySelector(".popup__edit_save");
+const popupPlusSave  = popupPlus.querySelector(".popup__plus_save");
+const popupEditClose  = popupEdit.querySelector(".popup__edit_close");
+const popupPlusClose  = popupPlus.querySelector(".popup__plus_close");
 const nameInputName = document.getElementById("name");
 const nameInputAvocation = document.getElementById("avocation");
 const nameSpan = document.querySelector(".profile__title");
 const avocationSpan = document.querySelector(".profile__subtitle");
 const form  = document.getElementById("submit-form");
-const likeBtnList = document.querySelectorAll(".elements__like");
-const openPopupTitleBtn = document.querySelector(".profile__plus");
 const nameInputTitle = document.getElementById("title");
 const nameInputLink = document.getElementById("link");
-const formTwo  = document.getElementById("place-form");
-const listItemTemplate = document.querySelector('#place').content.querySelector('elements__place');
+const formPlace  = document.getElementById("place-form");
+const listItemTemplate = document.querySelector('#place');
+const list = document.querySelector('.elements');
+const card = document.querySelector('.popup__card');
 
 
 const initialCards = [
   {
     name: 'Камчатка',
-    link: 'https://unsplash.com/photos/alZtq9SiRqI'
+    link: 'https://images.unsplash.com/photo-1634745186518-db2e653372c9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
   },
   {
     name: 'Алтай',
-    link: 'https://unsplash.com/photos/rZNBBaHp3jM'
+    link: 'https://images.unsplash.com/photo-1597241234507-fb03ab29199e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
   },
   {
     name: 'Карелия',
-    link: 'https://unsplash.com/photos/cg-rVevFtzU'
+    link: 'https://images.unsplash.com/photo-1602162991689-c97214fb18f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
   },
   {
     name: 'Смоленск',
-    link: 'https://unsplash.com/photos/d4_LqtEpXAk'
+    link: 'https://images.unsplash.com/photo-1603708200950-324b7933d332?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
   },
   {
     name: 'Дубровицы',
-    link: 'https://unsplash.com/photos/uUEkyg3XvmY'
+    link: 'https://images.unsplash.com/photo-1627358510286-6d244d028887?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
   },
   {
     name: 'Тулиновка',
-    link: 'https://unsplash.com/photos/QhNytqtOtSg'
+    link: 'https://images.unsplash.com/photo-1545674525-2bd19515ca01?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
   }
 ];
 
 
 const formSubmitHandler = (formEvent) => {
   formEvent.preventDefault();
-  popup.classList.remove("popup_opened");
+  popupEdit.classList.remove("popup_opened");
   const name = nameInputName.value;
   const avocation = nameInputAvocation.value;
   nameSpan.innerText = name;
   avocationSpan.innerText = avocation;
 };
 
-const formTwoSubmitHandler = (formTwoEvent) => {
-  formTwoEvent.preventDefault();
-  popup.classList.remove("popup_opened");
-  const title = nameInputTitle.value;
-  const link = nameInputLink.value;
-/*  titleSpan.innerText = title;
-  LinkSpan.innerText = link;*/
+const formPlaceSubmitHandler = (formPlaceEvent) => {
+  formPlaceEvent.preventDefault();
+  popupPlus.classList.remove("popup_opened");
 };
 
-
-const closePopup = () => {
-  popup.classList.remove("popup_opened");
-};
 
 const openPopup = () => {
-  popup.classList.add("popup_opened");
+  popupEdit.classList.add("popup_opened");
   const name = nameSpan.innerText;
   const avocation = avocationSpan.innerText;
   nameInputName.value = name;
   nameInputAvocation.value = avocation;
-};
+}; //  откртие первого попапа с именем и профессией
 
-const markActive = (mouseEvent) => {
-  mouseEvent.target.classList.add("elements__like_active");
-};
+const closePopupEdit = () => {
+  closeDialog(popupEdit);
+}; // закртыие по кнопке создать
 
-const openPopupTwo = () => {
-  popup.classList.add("popup_opened");
-/*  const title = titleSpan.innerText;
-  const link = linkSpan.innerText;*/
+const closeEdit = () => {
+  closeDialog(popupEdit);
+}; // закрытие по крестику
+
+const openPopupPlace = () => {
+  popupPlus.classList.add("popup_opened");
+/*  const title = null;
+  const link = null;
   nameInputTitle.value = title;
-  nameInputLink.value = link;
-};
+  nameInputLink.value = link;*/
+}; //  откртие второго попапа с фото и ссылкой
+
+const closePopupPlace = () => {
+  closeDialog(popupPlus);
+}; // закрытие по кнопке сохранить
+
+const closePlus = () => {
+  closeDialog(popupPlus);
+}; // закрытие по крестику
+
+const closeDialog = (dialog) => {
+  dialog.classList.remove("popup_opened")
+}; // закртие общее
 
 
-openPopupTitleBtn.addEventListener('click', openPopupTwo);
+
+openPopupTitleBtn.addEventListener('click', openPopupPlace);
 openPopupBtn.addEventListener("click", openPopup);
-popupCloseBtn.addEventListener("click", closePopup);
+popupPlusSave.addEventListener("click", closePopupPlace);
+popupEditSave.addEventListener("click", closePopupEdit);
+popupPlusClose.addEventListener("click", closePlus);
+popupEditClose.addEventListener("click", closeEdit);
 form.addEventListener("submit", formSubmitHandler);
-formTwo.addEventListener("submit", formTwoSubmitHandler);
-likeBtnList.forEach( (likeBtn) => {
-  likeBtn.addEventListener('click', markActive)
-});
+formPlace.addEventListener("submit", formPlaceSubmitHandler);
 
 
-
-const createItem = (item) => {
- const element = listItemTemplate.cloneNode(true);
- const elementName = element.querySelector('.elements__text');
- elementName.innerHTML = item.text;
-
- list.intertAdjacentHTML('beforeend', element);
+const renderCenterPane = () => {
+  for(let i = 0; i < initialCards.length; i++) {
+    createItem(initialCards[i], false);
+  }
 };
 
-initialCards.forEach(createItem);
+const createItem = (item, appendToStart) => {
+  const element = listItemTemplate.content.cloneNode(true);
+  const elementName = element.querySelector('.elements__text');
+  const elementImg = element.querySelector('.elements__photo');
+  const elementLike = element.querySelector('.elements__like')
+  const removeButton = element.querySelector('.elements__basket');
+  elementName.innerText = item.name; // меняет текст в заголовке
+  elementImg.attributes.src.nodeValue = item.link; // меняет ссылку на картинку
+  if (appendToStart) {
+    list.prepend(element)
+  } else {
+    list.appendChild(element)
+  }
+  const likeButton = () => {
+    elementLike.classList.toggle('elements__like_active')
+  }
+  elementLike.addEventListener('click', likeButton)
+
+  const removeCard = () => {
+   /* list.removeChild(element);*/
+    element.parentNode.removeChild(element)
+  }
+
+ removeButton.addEventListener('click', removeCard);
+};
+
+
+const handleFormSubmit = (event) => {
+  event.preventDefault(); // предотвращает перезагрузку страницы
+  createItem({
+    name: nameInputTitle.value,
+    link: nameInputLink.value
+  }, true);
+}
+ card.addEventListener('submit', handleFormSubmit);
+
+
+
+renderCenterPane()
