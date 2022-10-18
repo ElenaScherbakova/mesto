5ed05fb29124b47f6a81f4c989cd6e94942b0783
Elena Scherbakova <25pochta25@mail.ru>
2022-10-18 16:47:12 +0300

    добавлен фон у попапа фото

---
+++ scripts/cards.js
@@ -0,0 +1,26 @@
+const initialCards = [
+  {
+    name: 'Камчатка',
+    link: 'https://images.unsplash.com/photo-1634745186518-db2e653372c9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
+  },
+  {
+    name: 'Алтай',
+    link: 'https://images.unsplash.com/photo-1597241234507-fb03ab29199e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
+  },
+  {
+    name: 'Карелия',
+    link: 'https://images.unsplash.com/photo-1602162991689-c97214fb18f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+  },
+  {
+    name: 'Смоленск',
+    link: 'https://images.unsplash.com/photo-1603708200950-324b7933d332?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
+  },
+  {
+    name: 'Дубровицы',
+    link: 'https://images.unsplash.com/photo-1627358510286-6d244d028887?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+  },
+  {
+    name: 'Тулиновка',
+    link: 'https://images.unsplash.com/photo-1545674525-2bd19515ca01?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+  }
+];
--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 
-const openPopupBtn = document.querySelector(".profile__edit");
-const openPopupTitleBtn = document.querySelector(".profile__plus");
-const openPopupPhoto = document.querySelector(".popup_type_image");
+const buttonOpenEditPopup = document.querySelector(".profile__edit");
+const buttonOpenPlusPopup = document.querySelector(".profile__plus");
+const buttonOpenPhotoPopup = document.querySelector(".popup_type_image");
 const popupEdit = document.querySelector(".popup_type_edit");
 const popupPlus = document.querySelector(".popup_type_plus");
 const popupPhoto = document.querySelector('.popup__photo');
@@ -22,34 +22,9 @@ const listItemTemplate = document.querySelector('#place');
 const list = document.querySelector('.elements');
 const popupFigcaption = document.querySelector(".popup__figcaption")
 
-const initialCards = [
-  {
-    name: 'Камчатка',
-    link: 'https://images.unsplash.com/photo-1634745186518-db2e653372c9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
-  },
-  {
-    name: 'Алтай',
-    link: 'https://images.unsplash.com/photo-1597241234507-fb03ab29199e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
-  },
-  {
-    name: 'Карелия',
-    link: 'https://images.unsplash.com/photo-1602162991689-c97214fb18f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
-  },
-  {
-    name: 'Смоленск',
-    link: 'https://images.unsplash.com/photo-1603708200950-324b7933d332?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
-  },
-  {
-    name: 'Дубровицы',
-    link: 'https://images.unsplash.com/photo-1627358510286-6d244d028887?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
-  },
-  {
-    name: 'Тулиновка',
-    link: 'https://images.unsplash.com/photo-1545674525-2bd19515ca01?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
-  }
-];
 
-const formSubmitHandler = (formEvent) => {
+
+const handleSubmitProfileEditForm = (formEvent) => {
   formEvent.preventDefault();
   popupEdit.classList.remove("popup_opened");
   const name = nameInputName.value;
@@ -58,13 +33,18 @@ const formSubmitHandler = (formEvent) => {
   avocationSpan.innerText = avocation;
 };
 
-const formPlaceSubmitHandler = (formPlaceEvent) => {
+const handleSubmitProfilePlaceForm = (formPlaceEvent) => {
   formPlaceEvent.preventDefault();
   popupPlus.classList.remove("popup_opened");
 };
 
-const openPopup = () => {
-  popupEdit.classList.add("popup_opened");
+const openDialog = (dialog) => {
+  dialog.classList.remove('popup_hidden')
+  dialog.classList.add("popup_opened")
+};
+
+const openEditPopup = () => {
+  openDialog(popupEdit);
   const name = nameSpan.innerText;
   const avocation = avocationSpan.innerText;
   nameInputName.value = name;
@@ -80,7 +60,7 @@ const closeEdit = () => {
 }; // закрытие по крестику
 
 const openPopupPlace = () => {
-  popupPlus.classList.add("popup_opened");
+  openDialog(popupPlus)
 }; //  откртие второго попапа с фото и ссылкой
 
 const closePopupPlace = () => {
@@ -92,22 +72,23 @@ const closePlus = () => {
 }; // закрытие по крестику
 
 const closePhoto = () => {
-  closeDialog(openPopupPhoto);
+  closeDialog(buttonOpenPhotoPopup);
 };
 
 const closeDialog = (dialog) => {
   dialog.classList.remove("popup_opened")
+  dialog.classList.add('popup_hidden')
 }; // закртие общее
 
 popupPhotoClose.addEventListener('click', closePhoto);
-openPopupTitleBtn.addEventListener('click', openPopupPlace);
-openPopupBtn.addEventListener("click", openPopup);
+buttonOpenPlusPopup.addEventListener('click', openPopupPlace);
+buttonOpenEditPopup.addEventListener("click", openEditPopup);
 popupPlusSave.addEventListener("click", closePopupPlace);
 popupEditSave.addEventListener("click", closePopupEdit);
 popupPlusClose.addEventListener("click", closePlus);
 popupEditClose.addEventListener("click", closeEdit);
-form.addEventListener("submit", formSubmitHandler);
-formPlace.addEventListener("submit", formPlaceSubmitHandler);
+form.addEventListener("submit", handleSubmitProfileEditForm);
+formPlace.addEventListener("submit", handleSubmitProfilePlaceForm);
 
 const renderCenterPane = () => {
   for (let i = 0; i < initialCards.length; i++) {
@@ -122,7 +103,7 @@ const createItem = (item, appendToStart) => {
   const elementLike = element.querySelector('.elements__like')
   const removeButton = element.querySelector('.elements__basket');
   elementName.innerText = item.name; // меняет текст в заголовке
-  elementImg.attributes.src.nodeValue = item.link; // меняет ссылку на картинку
+  elementImg.src = item.link; // меняет ссылку на картинку
   if (appendToStart) {
     list.prepend(element)
   } else {
@@ -141,7 +122,7 @@ const createItem = (item, appendToStart) => {
     popupPhoto.setAttribute('alt', item.name)
     popupPhoto.setAttribute('src', item.link);
     popupFigcaption.textContent = item.name;
-    openPopupPhoto.classList.add("popup_opened");
+    openDialog(buttonOpenPhotoPopup)
   }) // вызов модалки с фото
 
   removeButton.addEventListener('click', removeCard);
@@ -154,6 +135,5 @@ const handleFormSubmit = (event) => {
     link: nameInputLink.value
   }, true);
 }
-formPlace.addEventListener('submit', handleFormSubmit);
 
 renderCenterPane()
